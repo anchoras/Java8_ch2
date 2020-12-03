cb2e5415d65e7261d68e4589bd215511071a16b3
anchoras <anchoras2@gmail.com>
2020-12-03 16:05:58 +0300

    first example changed to calculating sqrt from two numbers (or hypotenuse length)

--- InchesVolume.java
+++ InchesVolume.java
@@ -1,8 +1,15 @@
 class InchesVolume {
     public static void main (String args[]) {
-        long ci, im;
+        /*long ci, im;
         im = 5280 * 12;
         ci = im * im * im;
         System.out.println("There are " + ci + " cubic inches in 1 cubic mile");
+        */
+        double x, y, z;
+        x = 3;
+        y = 4;
+        z = Math.sqrt(x*x + y*y);
+
+        System.out.println("Length of Hypotenuse is " + z);
     }
 }
