97faaba6b4de5f25dd92e2f1a9d20818e235c1c7
anchoras <anchoras2@gmail.com>
2020-12-03 15:57:55 +0300

    first example in this chapter: cubic mile to cubic inches

---
+++ InchesVolume.java
@@ -0,0 +1,8 @@
+class InchesVolume {
+    public static void main (String args[]) {
+        long ci, im;
+        im = 5280 * 12;
+        ci = im * im * im;
+        System.out.println("There are " + ci + " cubic inches in 1 cubic mile");
+    }
+}
