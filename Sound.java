cf5b0dc94f06a636069c37c8bc7cf31051a2a43e
anchoras <anchoras2@gmail.com>
2020-12-09 16:42:31 +0300

    added program calculating distance to lighting took place

---
+++ Sound.java
@@ -0,0 +1,14 @@
+/*
+    Sound.java
+    Program for calculating distance from lighting
+    by difference in time from light to sound came
+*/
+
+class Sound {
+    public static void main (String args[]) {
+        double dist;
+        dist = 1100 * 7.2;  // 7.2 sec
+
+        System.out.println("The distance between lighting took place " + "is " + dist + "feet");
+    }
+}
