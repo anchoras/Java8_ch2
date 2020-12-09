1d60656619db3a45e29515f14ac53e119d560beb
anchoras <anchoras2@gmail.com>
2020-12-09 16:46:03 +0300

    Modified distance calc program for echo-timing

--- Sound.java
+++ Sound.java
@@ -1,14 +1,14 @@
 /*
     Sound.java
-    Program for calculating distance from lighting
-    by difference in time from light to sound came
+    Program for calculating distance from big object
+    by difference in time sound goes and echo returns
 */
 
 class Sound {
     public static void main (String args[]) {
         double dist;
-        dist = 1100 * 7.2;  // 7.2 sec
+        dist = (1100 * 7.2)/2;  // 7.2 sec
 
-        System.out.println("The distance between lighting took place " + "is " + dist + "feet");
+        System.out.println("The distance between *me* and the object " + "is " + dist + "feet");
     }
 }
