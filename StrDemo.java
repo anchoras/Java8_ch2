584180ff0d423e16b35beb03bd1a5637d99ffac4
anchoras <anchoras2@gmail.com>
2020-12-09 21:17:18 +0300

    added file for demo string literals

---
+++ StrDemo.java
@@ -0,0 +1,12 @@
+/*
+    demo file for demonstration of string literals
+    StrDemo
+*/
+
+class StrDemo {
+    public static void main (String args[]) {
+        System.out.println("First string\nSecond string");
+        System.out.println("A\tB\tC");
+        System.out.println("D\tE\tF");
+    }
+}
